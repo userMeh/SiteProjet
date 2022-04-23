01096f43734ca7333491bfb0d508e52069e38750
userMeh <szhang0709@gmail.com>
2022-04-23 03:49:53 +0200

    nettoyage + rearrangement

--- includes/header.php
+++ includes/header.php
@@ -105,20 +105,7 @@
             <li>
               <buttom  onclick="darkMode()"><img src="images/moon.png" class="moon"></buttom>
             </li>
-            <script>
-            function darkMode() {
-              var element = document.body;
-              var content = document.getElementById("DarkModetext");
-              element.className = "dark-mode";
-              content.innerText = "Dark Mode is ON";
-            }
-            function lightMode() {
-              var element = document.body;
-              var content = document.getElementById("DarkModetext");
-              element.className = "light-mode";
-              content.innerText = "Dark Mode is OFF";
-            }
-            </script>
+            <script src="css-js/script.js"></script>
           </ul>
         </div>
       </div>
