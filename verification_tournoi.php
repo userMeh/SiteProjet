b6e5ce6329fdf7261a2f536dfaaf13ed02eb73b8
minatoco <102216647+minatoco@users.noreply.github.com>
2022-05-13 09:49:10 +0200

    Add files via upload

--- verification_tournoi.php
+++ verification_tournoi.php
@@ -1,12 +1,37 @@
 <?php
 
-setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
 
-
-ini_set('display_errors', 1);
-
+setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']); 
 
 include ('includes/bdd.php');
+
+
+
+
+
+if(isset($_GET['delete'])){
+
+  $tournoi = str_replace("-"," ",$_GET['delete']);               //Pour remettre les espaces a la place des - pour les retrouver dans la bdd
+
+  $query = $bdd->query('SELECT id FROM TOURNOI WHERE nom_du_jeu="'.$tournoi.'"');
+  $id = $query -> fetchAll(PDO::FETCH_COLUMN);
+
+  var_dump($id);
+
+
+  $bdd->query('DELETE FROM PARTICIPATION WHERE id="'.$id[0].'"');   //Pour supprimer les donnees de la table GENRE_JEUX
+  $bdd->query('DELETE FROM TOURNOI WHERE id="'.$id[0].'"');              //sinon on peut pas a cause de la cle etrangere
+
+  unlink('imagetournoi/'.$tournoi.'0.jpg');
+
+  $message="succès suppression jeu";
+  header('location:liste_tournoi.php?message='.$message);
+  exit;
+
+}
+
+
+
 
 $doublon=$bdd->prepare('SELECT nom_du_jeu FROM TOURNOI WHERE nom_du_jeu=?');
 $doublon->execute([
@@ -19,49 +44,19 @@ if(count($result) != 0){      //Pour voir si le tableau est pas vide
   header('location:ajout_tournoi.php?message='.$message);
   exit;
 }
-
-  $date = time();
-  $date_de_depart = strtotime($_POST['date_de_depart']);
-  $date_de_fin = strtotime($_POST['duree']);
-
-
-  $date_now= strftime('%d %B %Y', $date);
-  $date_de_debut = strftime('%d %B %Y', $date_de_depart);
-  $duree = strftime('%d %B %Y', $date_de_fin);
-
-
-
-  if ($date_de_depart > $date_de_fin || $date > $date_de_depart ) {
+  $date = date('d-m-y');
+  if ($_POST['date_de_depart'] > $_POST['duree'] && $date < $_POST['date_de_depart'] ) {
     $message="Les informations de la date de depart ou la date de fin sont invalides";
     header('location:ajout_tournoi.php?message='.$message);
     exit;
   }
+
 
     $uploads = 'imagetournoi';
 
     if(!file_exists($uploads)){
       mkdir($uploads,0777);
     }
-
-    // Charge le cachet et la photo afin d'y appliquer le tatouage numérique
-    $logo = imagecreatefrompng('images/Logic.logo.png');
-    $imagePrincipale = imagecreatefrompng($_FILES['imagePrincipale']['name']);
-
-    var_dump($logo);
-    var_dump($imagePrincipale);
-    exit;
-
-// Définit les marges pour le cachet et récupère la hauteur et la largeur de celui-ci
-  $marge_right = 10;
-  $marge_bottom = 10;
-  $sx = imagesx($logo);
-  $sy = imagesy($logo);
-
-// Copie le cachet sur la photo en utilisant les marges et la largeur de la
-// photo originale  afin de calculer la position du cachet
-  imagecopy($imagePrincipale, $logo, imagesx($imagePrincipale) - $sx - $marge_right, imagesy($imagePrincipale) - $sy - $marge_bottom, 0, 0, imagesx($logo), imagesy($logo));
-
-
 
     $imagePrincipale = $_FILES['imagePrincipale']['name'];
     $array = explode('.', $imagePrincipale);
@@ -70,7 +65,7 @@ if(count($result) != 0){      //Pour voir si le tableau est pas vide
     $destination = $uploads . '/' . $imagePrincipale;
     move_uploaded_file($_FILES['imagePrincipale']['tmp_name'], $destination);
 
-    var_dump($_FILES);
+
 
 
 
